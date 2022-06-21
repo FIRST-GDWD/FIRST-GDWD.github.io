ec0c97fec03af5272b91cee0df6365b1f2f492c2
Ryan R <ryan.reid@first.edu>
2022-06-20 23:21:49 -0400

    hamburger and php

---
+++ examples/week8/Day29_php_intro.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <title>PHP Example</title>
+  </head>
+  <body>
+    <h1>PHP code example</h1>
+
+    <?php
+      echo "Hello Ryan!";
+    ?>
+  </body>
+</html>
