c4aea4dc333f2139d69e680b8e3ccc9fac916523
Ryan R <ryan.reid@first.edu>
2023-02-23 16:35:30 -0500

    js demo

---
+++ examples6010/js/primitiveDataTypes.js
@@ -0,0 +1,21 @@
+// console.log will output anything
+// you put in the parentheses
+// to the Chrome Developer Tools' Console
+
+// Data Type: String (text)
+console.log("Hello World!");
+
+// Data Type: Number
+console.log(4);
+console.log(10000);
+
+// Math operators 
+// addition, subtraction, multiplication, division
+console.log(10 + 5);
+console.log(20 - 12);
+console.log(3 * 4);
+console.log(10 / 2);
+
+// String operators
+// concatenation (putting two strings together)
+console.log("Hello" + "World?");
