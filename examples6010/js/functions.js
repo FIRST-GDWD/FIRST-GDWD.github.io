694123593cb3cbd8f40eb11886062c96e0b536e8
Ryan R <ryan.reid@first.edu>
2023-02-28 17:07:34 -0500

    ++

--- examples6010/js/functions.js
+++ examples6010/js/functions.js
@@ -1,10 +1,15 @@
+// Variable Review
 var greeting = "Good evening";
 console.log(greeting);
 
+// Simple function
 function greet() {
     console.log(greeting);
 }
 
+greet();
+
+// Function with input parameter
 function greetUser(username) {
     console.log(greeting + ", " + username);
 }
@@ -13,6 +18,8 @@ greetUser("Ryan");
 greetUser("Jamir");
 greetUser("symphorian");
 
+// Function with input parameter
+// that modifies the page
 function greetUserOnPage(username) {
     let greetingDisplay = document.getElementById("greetingDisplay");
     greetingDisplay.innerHTML = greeting + ", " + username;
@@ -20,6 +27,7 @@ function greetUserOnPage(username) {
 
 greetUserOnPage("Ryan");
 
+// Function that outputs (or "returns") a value
 function getUserGreeting(username) {
     return greeting + ", " + username + "!";
 }
@@ -27,6 +35,8 @@ function getUserGreeting(username) {
 getUserGreeting("ryan");
 console.log(getUserGreeting("ryan"));
 
+// Function using another function
+// that modifies the page
 function greetUserOnPageV2(username) {
     //let greetingDisplay = document.getElementById("greetingDisplay");
     greetingDisplay.innerHTML = getUserGreeting(username);
