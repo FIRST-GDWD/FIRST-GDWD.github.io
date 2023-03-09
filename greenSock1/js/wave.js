e264bbe86a6c9ba04598e85c95b7fc6c144dfafa
Ryan R <ryan.reid@first.edu>
2023-03-08 23:18:45 -0500

    gsap++

--- greenSock1/js/wave.js
+++ greenSock1/js/wave.js
@@ -1,13 +1,14 @@
-gsap.to(".box", 1, {
-    scale: 0.1, 
+gsap.to(".box", 0.5, {
+    scale: 0.5,
+    backgroundColor: "red",
     y: 60,
-    yoyo: true, 
-    repeat: -1, 
-    ease: "power1.inOut",
+    yoyo: true,
+    repeat: -1,
+    ease: "bounce",
     delay: 1,
     stagger: {
-      amount: 1.5, 
-      grid: "auto",
-      from: "center"
-    }
-  });
+        amount: 1.5,
+        grid: "auto",
+        from: "center",
+    },
+});
