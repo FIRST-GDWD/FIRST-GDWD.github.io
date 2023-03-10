24e987eba05da622f9893b5630bc25a4119939aa
Ryan R <ryan.reid@first.edu>
2023-03-09 23:24:31 -0500

    last examples

--- examples6010/js/lastExample.js
+++ examples6010/js/lastExample.js
@@ -1,6 +1,10 @@
 const flexbox2 = document.getElementById("flexbox2");
 const flexbox3 = document.getElementById("flexbox3");
 const grid = document.getElementById("grid");
+const heroLink = document.getElementById("heroLink");
+const flexboxGridLink =
+    document.getElementById("flexboxGridLink");
+const gridLink = document.getElementById("gridLink");
 
 function hideAll() {
     flexbox2.style.display = "none";
@@ -13,18 +17,16 @@ function showHero() {
     hideAll();
     flexbox2.style.display = "";
 }
+heroLink.onclick = showHero;
+
 function showFlexboxGrid() {
     hideAll();
     flexbox3.style.display = "";
 }
+flexboxGridLink.onclick = showFlexboxGrid;
+
 function showGrid() {
     hideAll();
     grid.style.display = "";
 }
-
-const heroLink = document.getElementById("heroLink");
-heroLink.onclick = showHero;
-const flexboxGridLink = document.getElementById("flexboxGridLink");
-flexboxGridLink.onclick = showFlexboxGrid;
-const gridLink = document.getElementById("gridLink");
 gridLink.onclick = showGrid;
