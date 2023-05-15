672871ae41b6e8272c72e853ac4aaee099382dd1
Ryan R <ryan.reid@first.edu>
2023-05-15 16:58:31 -0400

    js data types

---
+++ examples/js/primitive_data_types.js
@@ -0,0 +1,19 @@
+console.log("Hello World!!!");
+
+console.log(10000);
+console.log("4");
+
+console.log(10 + 5);
+console.log(20 - 12);
+console.log(3 * 4);
+console.log(10 / 2);
+
+// Boolean data type (true/false)
+console.log(true);
+console.log(false);
+
+console.log(10 == 10);
+console.log(10 == 5);
+
+console.log(10 != 10);
+console.log(10 != 5);
