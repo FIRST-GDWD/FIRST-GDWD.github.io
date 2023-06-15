4a02b2d12123c0c71b7f5287a6d22f059b4b7cd9
Ryan R <ryan.reid@first.edu>
2023-06-15 18:49:49 -0400

    text styling and lists part 1

--- cookbook/js/meal-prep.js
+++ cookbook/js/meal-prep.js
@@ -6,7 +6,15 @@ const mealPrepRecipes = [
     {
         recipeName: "headings",
         pageCount: 4,
-    }
+    },
+    {
+        recipeName: "text-styling",
+        pageCount: 8,
+    },
+    {
+        recipeName: "lists",
+        pageCount: 6,
+    },
 ];
 const ROOT_RECIPE_FOLDER = "meal-prep";
 
