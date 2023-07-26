a32c869d0583ec13bb12804645f3ef2b65339d54
Ryan R <ryan.reid@first.edu>
2023-07-26 17:39:11 -0400

    recipe 1 - single column (margin)

---
+++ cookbook/js/recipes.js
@@ -0,0 +1,16 @@
+/***********************************************************************
+ * variables for dynamic content
+ **********************************************************************/
+const recipeNames = [];
+const mealPrepRecipes = [
+    {
+        recipeName: "single-column-center-margin",
+        categories: "page-layout, single-column",
+        pageCount: 3,
+        includesLocalCSS: true,
+    },
+];
+const ROOT_RECIPE_FOLDER = "recipes";
+const IS_DYNAMIC_LOAD = true;
+
+// load cookbook.js immediately after this file
