0e5ca0560645545e31b9a98717b5437970ab1521
Unique <ryan.reid@first.edu>
2023-07-26 19:05:12 -0400

    single column layouts

--- cookbook/js/recipes.js
+++ cookbook/js/recipes.js
@@ -9,6 +9,18 @@ const mealPrepRecipes = [
         pageCount: 3,
         includesLocalCSS: true,
     },
+    {
+        recipeName: "single-column-left",
+        categories: "page-layout, single-column",
+        pageCount: 2,
+        includesLocalCSS: true,
+    },
+    {
+        recipeName: "single-column-right",
+        categories: "page-layout, single-column",
+        pageCount: 2,
+        includesLocalCSS: true,
+    },
 ];
 const ROOT_RECIPE_FOLDER = "recipes";
 const IS_DYNAMIC_LOAD = true;
