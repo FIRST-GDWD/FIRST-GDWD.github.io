7176d2da382d1d23dd573bba1c1cbeaa1d93ebaa
Ryan R <ryan.reid@first.edu>
2023-08-01 17:40:31 -0400

    three columns sidebars

--- cookbook/js/recipes.js
+++ cookbook/js/recipes.js
@@ -23,20 +23,26 @@ const mealPrepRecipes = [
     },
     {
         recipeName: "two-columns-sidebar-flex",
-        categories: "page-layout, two-columns",
+        categories: "page-layout, two-columns, flex",
         pageCount: 4,
         includesLocalCSS: true,
     },
     {
         recipeName: "two-columns-equal-flex",
-        categories: "page-layout, two-columns",
+        categories: "page-layout, two-columns, flex",
         pageCount: 3,
         includesLocalCSS: true,
     },
     {
         recipeName: "two-columns-sidebar-grid",
-        categories: "page-layout, two-columns",
+        categories: "page-layout, two-columns, grid",
         pageCount: 4,
+        includesLocalCSS: true,
+    },
+    {
+        recipeName: "three-columns-sidebars-grid",
+        categories: "page-layout, two-columns, grid",
+        pageCount: 3,
         includesLocalCSS: true,
     },
 ];
