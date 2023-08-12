3ef7c9521b57a83cad9f71f76f41f2b729eb8627
Ryan R <ryan.reid@first.edu>
2023-08-11 22:23:44 -0400

    three column layout grid (and 2col improvement)

--- cookbook/js/meal-prep.js
+++ cookbook/js/meal-prep.js
@@ -178,6 +178,12 @@ const mealPrepRecipes = [
         pageCount: 9,
         includesLocalCSS: true,
     },
+    {
+        recipeName: "three-column-grid-layouts",
+        categories: "demo, week3",
+        pageCount: 9,
+        includesLocalCSS: true,
+    },
 ];
 const ROOT_RECIPE_FOLDER = "meal-prep";
 const IS_DYNAMIC_LOAD = true;
