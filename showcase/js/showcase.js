9a25c2540fac3851706fc6134a5a1515e01f612a
Ryan R <ryan.reid@first.edu>
2025-06-17 08:58:28 -0400

    more showcases

--- showcase/js/showcase.js
+++ showcase/js/showcase.js
@@ -1,7 +1,7 @@
 const showcaseURLs = [
     "https://sammiballs.github.io/projects/alley-posters/",
     "https://quisdofeles.github.io/projects/alley-posters/",
-    "http://liliannaboyer.github.io/projects/studio-gallery-v2/",
+    "https://liliannaboyer.github.io/projects/studio-gallery-v2/",
     "https://evaortiz71.github.io/projects/studio-gallery-v2/",
     "https://antionejh.github.io/2025/projects/studio-gallery-v2/",
     "https://shamariaking7.github.io/projects/studio-gallery-v2/",
@@ -12,7 +12,10 @@ const showcaseURLs = [
     "https://dstyen.github.io/projects/studio-gallery-v2/",
     "https://deontwitty.github.io/projects/studio-gallery-v2/index.html",
     "https://sheilawilliamson37.github.io/sheilawilliamson37.github.io-skeleton-code-/projects/studio-gallery-v2/",
-    "https://mzuroff.github.io/projects/studio-gallery-v2/"
+    "https://mzuroff.github.io/projects/studio-gallery-v2/",
+    "https://gassedbeauty.github.io/projects/alley-posters-v2/",
+    "https://johnnyhudson5.github.io/projects/studio-gallery-v2/",
+
 ];
 const gallery = document.getElementById("gallery");
 const backWall = document.getElementById("backWall");
