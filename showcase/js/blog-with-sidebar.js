1da381333f4e023493602f583f6e2616a1c30649
Ryan R <ryan.reid@first.edu>
2025-06-19 15:51:05 -0400

    6026 blog showcase

--- showcase/js/blog-with-sidebar.js
+++ showcase/js/blog-with-sidebar.js
@@ -1,9 +1,17 @@
 const showcaseURLs = [
     "https://first-gdwd.github.io/cookbook/examples/blog-with-sidebar/",
-    "https://kiayambenavides.github.io/exercises/blog-with-sidebar-exercise/",
-    "https://malis86.github.io/exercises/blog-with-sidebar/",
-    "https://gray-whale-graphics.github.io/exercises/blog-with-sidebar/",
-    "https://trelonda89.github.io/exercises/Blog-with-sidebar/",
+    "https://liliannaboyer.github.io/exercises/blog-with-sidebar/",
+    "https://evaortiz71.github.io/exercises/blog-with-sidebar/",
+    "https://antionejh.github.io/2025/exercises/code-skeleton%20copy%202/",
+    "https://shamariaking7.github.io/exercises/blog-with-site/",
+    "https://saga-laha.github.io/exercises/blog-with-sidebar/",
+    "https://aaroncortez764.github.io/aaroncortez769.github.io/exercises/blog-with-sidebar/",
+    "https://tatyanna-first.github.io/exercicses/blog-with-sidebar/",
+    "https://moonstar25.github.io/exercises/blog-with-sidebar/",
+    "https://amarismth.github.io/excercises/blog-with-sidebar/",
+    "https://dstyen.github.io/exercises/blog-with-sidebar/",
+    "https://deontwitty.github.io/exercise%20/blog-with-sidebar/index.html",
+    "https://ccolbae.github.io/exercises/blog-post/",
 ];
 
 const labelText = "View Blog Post";
