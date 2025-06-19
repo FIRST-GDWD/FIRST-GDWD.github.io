1cb9ccb68919e2661626280e7fc7990cbf7bcb05
Ryan R <ryan.reid@first.edu>
2025-06-19 12:44:23 -0400

    blog showcase

---
+++ showcase/js/blog-with-sidebar.js
@@ -0,0 +1,10 @@
+const showcaseURLs = [
+    "https://kiayambenavides.github.io/exercises/blog-with-sidebar-exercise/",
+    "https://malis86.github.io/exercises/blog-with-sidebar/",
+    "https://gray-whale-graphics.github.io/exercises/blog-with-sidebar/",
+    "https://trelonda89.github.io/exercises/Blog-with-sidebar/",
+];
+
+const labelText = "View Blog Post";
+const showcaseWidth = 225;
+const zCloseness = 490;
