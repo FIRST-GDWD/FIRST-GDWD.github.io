cb3a44a9f25b9f9012fafd077934efa302fdb272
Ryan R <ryan.reid@first.edu>
2025-06-20 16:19:45 -0400

    mebay showcase

---
+++ showcase/js/mebay-product-page.js
@@ -0,0 +1,14 @@
+const showcaseURLs = [
+    "https://first-gdwd.github.io/cookbook/examples/mebay-product-page-v2/",
+    "https://kiayambenavides.github.io/projects/mebay/",
+    "https://malis86.github.io/projects/mebay-product-page/",
+    "https://gray-whale-graphics.github.io/projects/mebay-product-page/",
+    "https://trelonda89.github.io/projects/mebay-product-page/",
+    "https://scoodlese.github.io/projects/mebay/",
+    "https://xavionroseboro.github.io/projects/mebay-product-page/index.html",
+    "https://kay678.github.io/mebay-product-page/",
+];
+
+const labelText = "View Product Page";
+const showcaseWidth = 425;
+const zCloseness = 490;
