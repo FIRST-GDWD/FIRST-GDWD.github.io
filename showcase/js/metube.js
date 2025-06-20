e2e408c7c72f56e7d18d07d8bfd8c71e579863be
Ryan R <ryan.reid@first.edu>
2025-06-20 09:48:57 -0400

    showcase updates

---
+++ showcase/js/metube.js
@@ -0,0 +1,13 @@
+const showcaseURLs = [
+    "https://first-gdwd.github.io/cookbook/examples/metube/",
+    "https://kiayambenavides.github.io/exercises/metube-exercise-part2/",
+    "https://gray-whale-graphics.github.io/exercises/metube/",
+    "https://treasurejones.github.io/metube/",
+    "https://trelonda89.github.io/demos/metube/",
+    "https://scoodlese.github.io/exercises/me-tube/",
+    "https://xavionroseboro.github.io/demos/list-demo/two-columns-mixed/index.html",
+];
+
+const labelText = "View MeTube Page";
+const showcaseWidth = 425;
+const zCloseness = 490;
