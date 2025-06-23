ac97add7026a5931eb35b8069dbd5c2ea6a89164
Ryan R <ryan.reid@first.edu>
2025-06-23 08:54:34 -0400

    more metube

--- showcase/js/metube.js
+++ showcase/js/metube.js
@@ -1,9 +1,13 @@
 const showcaseURLs = [
     "https://first-gdwd.github.io/cookbook/examples/metube/",
+    "https://zasha349.github.io/exercise/metube/",
     "https://kiayambenavides.github.io/exercises/metube-exercise-part2/",
+    "https://rainbowcheesewedge.github.io/exercises/two-columns-mixed(metube)/index.html",
     "https://gray-whale-graphics.github.io/exercises/metube/",
     "https://treasurejones.github.io/metube/",
+    "https://teyebourke.github.io/demos/14-two-column-mix/",
     "https://trelonda89.github.io/demos/metube/",
+    "https://pushinglotuses.github.io/demos/14-two-column-mixed/",
     "https://scoodlese.github.io/exercises/me-tube/",
     "https://xavionroseboro.github.io/demos/list-demo/two-columns-mixed/index.html",
 ];
