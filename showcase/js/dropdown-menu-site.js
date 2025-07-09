bcaca484e1606978f4f2fc2a0a45b04524185f72
Ryan R <ryan.reid@first.edu>
2025-07-09 13:57:01 -0400

    dropdown site examples

---
+++ showcase/js/dropdown-menu-site.js
@@ -0,0 +1,15 @@
+const showcaseURLs = [
+    "https://first-gdwd.github.io/cookbook/examples/adopt-me-plz/",
+    "https://gray-whale-graphics.github.io/projects/dropdown-menu-site/index.html",
+    "https://sodiumh20.github.io/projects/Dropdown/index.html",
+    "https://scoodlese.github.io/projects/dropdown-menu-site/index.html",
+    "https://kay678.github.io/dropdown-menu-site/index.html",
+    "https://rainbowcheesewedge.github.io/projects/dropdown-menu/index.html",
+    "https://zasha349.github.io/projects/AdoptMePlz/index.html",
+    "https://tjnetwork.github.io/projects/dropdown-menu-site/index.html",
+    "https://melissazarama.github.io/projects/dropdown-menu-site/index.html",
+];
+
+const labelText = "Visit AdoptMePlz!";
+const showcaseWidth = 425;
+const zCloseness = 490;
