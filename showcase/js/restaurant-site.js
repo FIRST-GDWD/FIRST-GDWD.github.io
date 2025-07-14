408265aabb2741550141650690a1c3a10a97baf1
Ryan R <ryan.reid@first.edu>
2025-07-14 09:41:49 -0400

    restaurant showcase

---
+++ showcase/js/restaurant-site.js
@@ -0,0 +1,15 @@
+const showcaseURLs = [
+    "https://first-gdwd.github.io/cookbook/examples/restaurant/",
+    "https://richardmahadeo.github.io/projects/Final-Project-resteruant_site/",
+    "https://sodiumh20.github.io/projects/restaurant/",
+    "https://rainbowcheesewedge.github.io/projects/restaurant/index.html",
+    "https://tylermazzuca121.github.io/projects/restaurant/",
+    "https://kiayambenavides.github.io./exercises/restaurant/index.html",
+    "https://zasha349.github.io/projects/resturant/index.html",
+    "https://melissazarama.github.io/exercises/restaurant/",
+    "https://kiaraimani.github.io/projects/restaurant/",
+    "https://gray-whale-graphics.github.io/projects/restaurant/index.html",
+];
+const labelText = "Visit Restaurant Site";
+const showcaseWidth = 425;
+const zCloseness = 490;
