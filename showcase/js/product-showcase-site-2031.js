4e2ecc998666230841aa7709f2df518a37be8253
Ryan R <ryan.reid@first.edu>
2026-02-17 09:25:05 -0500

    2031 showcase

---
+++ showcase/js/product-showcase-site-2031.js
@@ -0,0 +1,15 @@
+const showcaseURLs = [
+    "https://starartistgrl25.github.io/projects/product-showcase/",
+    "https://ced311.github.io/projects/3-product-showcase-site/",
+    "https://maybenhaj.github.io/exercises/product-showcase-site/",
+    "https://coding416.github.io/projects/product-showcase-site/",
+    "https://a24nneliese.github.io/projects/04-product-showcase-site/index.html",
+    "https://srivera-bit.github.io/project/product-showcase-site/index.html",
+    "https://alyd3zigns.github.io/projects/Product-showcase/",
+    "https://starboy1-design.github.io/projects/product-showcase-site/",
+    "https://alvalent94.github.io/projects/product-showcase-site/",
+];
+
+const labelText = "View Product Showcase";
+const showcaseWidth = 225;
+const zCloseness = 490;
